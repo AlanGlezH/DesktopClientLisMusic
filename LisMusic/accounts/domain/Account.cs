a455d8482bfbac1cd5089aa4634ca5db3b167bc3
AlanGlezH <alan14_04@hotmail.com>
2020-07-11 23:54:41 -0500

    Rest services: loginAccount and register account, added

---
+++ LisMusic/accounts/domain/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LisMusic.accounts.resources
+{
+    public class Account
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string userName { get; set; }
+        public string gender { get; set; }
+        public string birthday { get; set; }
+        public string cover { get; set; }
+        public string created { get; set; }
+        public string updated { get; set; }
+        public bool contentCreator { get; set; }
+        public string password { get; set; }
+        public string typeRegister { get; set; }
+    }
+}
---
+++ LisMusic/accounts/domain/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LisMusic.accounts.resources
+{
+    class LoginRequest
+    {
+        public String user { get; set; }
+        public String password { get; set; }
+    }
+}
