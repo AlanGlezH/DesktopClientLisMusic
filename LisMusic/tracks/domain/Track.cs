73912b095cd00f8bac3c0a110e1e7ae4b171aced
AlanGlezH <alan14_04@hotmail.com>
2020-07-18 16:52:04 -0500

    field track duration modified

--- LisMusic/tracks/domain/Track.cs
+++ LisMusic/tracks/domain/Track.cs
@@ -15,16 +15,19 @@ namespace LisMusic.tracks.domain
         public bool avaible { get; set; }
         public int indexRow { get; set; }
         public string artist_name { get; set; }
+
+        public string duration { get; set; }
         public string albumTitle { get; set; }
         public string artistName { get; set; }
 
 
 
-        public Track(string idTrack, string title, int reproductions, string fileTrack, bool avaible, string artist_name)
+        public Track(string idTrack, string title, double duration, int reproductions, string fileTrack, bool avaible, string artist_name)
         {
             this.idTrack = idTrack;
             this.title = title;
-            this.reproductions = reproductions;
+            this.reproductions = reproductions; TimeSpan result = TimeSpan.FromSeconds(duration);
+            this.duration = result.ToString("mm':'ss"); ;
             this.fileTrack = fileTrack;
             this.avaible = avaible;
             this.artist_name = artist_name;
